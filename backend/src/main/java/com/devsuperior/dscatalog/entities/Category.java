f5806c03a487cb79b907f7779c8642aef5eb8ef4
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-07-05 00:28:54 -0300

    Product entity, SQL script

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -18,7 +18,7 @@ public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)  /* transforma o id em auto incrementável*/
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
