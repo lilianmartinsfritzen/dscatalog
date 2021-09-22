d80a947859111dfe8df611c0a614b4fe31c33bf2
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-09-22 12:26:39 -0300

    Simulating Save, FindAll and FindById behaviors with Mockito.

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,19 +1,27 @@
 package com.devsuperior.dscatalog.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscatalog.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -27,21 +35,33 @@ public class ProductServiceTests {
 	private long existingId;
 	private long nonExistingId;
 	private long dependentId;
+	private PageImpl<Product> page;  // PageImpl é um tipo concreto que representa uma página de dados, usado nos testes.
+	private Product product;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
-		nonExistingId = 1000L;
-		dependentId = 4L;
+		nonExistingId = 2L;
+		dependentId = 3L;
+		product = Factory.createProduct();
+		page = new PageImpl<>(List.of(product));
+		
+		// Quando o método retorna algo (não é void) primeiro teremos o .when, depois a ação .doXxxx
+		// Quando passamos um argumento para uma chamada de método simulado, muitas vezes passamos um argumento qualquer por ser uma operação mais simples.
+		// não importa o argumento que está sendo recebido pela chamada, por isso vamos usar uma classe do Mockito chamada ArgumentMatchers., desta forma podemos simular a entrada de um valor qualquer do tipo que quisermos.
+		// No caso do findAll precisamos informar o tipo específico que queremos passar, fazemos então um caching passando a informação entre (), exemplo findAll((Pageable) ...) 
+		Mockito.when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
+		Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+		
+		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
+		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
 		
 		// É possível subtrair a palavra Mockito chamando diretamente o método, exemplo doNothing, fazendo isso é necessário realizar os imports estáticos da bliblioteca.
 		// Confirguração de comportamento simulado chamando o Mockito
-		Mockito.doNothing().when(repository).deleteById(existingId);
-		
-		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
-		
-		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
-		
+		// Quando um método é void primeiro colocamos a ação .doXxxx depois o .when
+		Mockito.doNothing().when(repository).deleteById(existingId);		
+		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);		
+		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);	
 	}
 	
 	@Test
