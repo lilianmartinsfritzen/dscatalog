56a3aa92646014199d685c24a64cbe593982ecf9
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-09-23 11:45:41 -0300

    FindAllPaged return Page when sort by name. Testado.

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIntegrationTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIntegrationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
@@ -67,13 +68,26 @@ public class ProductServiceIntegrationTests {
 	}
 	
 	@Test
-	public void findAllPagedShouldReturnEmptyWhenPageDoesNotExists() {
+	public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExists() {
 
 		PageRequest pageRequest = PageRequest.of(50, 10);
 		
 		Page<ProductDTO> result = service.findAllPaged(pageRequest);
 
 		Assertions.assertTrue(result.isEmpty());
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnSortedPageWhenSortByName() {
+
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
 	}
 }
 
