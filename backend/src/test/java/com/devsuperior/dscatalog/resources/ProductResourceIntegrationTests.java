631c51c6b6a4a1269dab8b0f8902d28b9eada0f3
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-09-23 12:19:49 -0300

    Update, integration Test on the web layer with existing Id.

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIntegrationTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIntegrationTests.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.resources;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,6 +15,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
@@ -22,9 +27,13 @@ public class ProductResourceIntegrationTests {
 	@Autowired
 	private MockMvc mockMvc;
 	
+	@Autowired  
+	private ObjectMapper objectMapper;
+	
 	private Long existingId;
 	private Long nonExistingId;
 	private Long countTotalProducts;
+	private ProductDTO productDTO;
 	
 	
 	@BeforeEach
@@ -32,6 +41,7 @@ public class ProductResourceIntegrationTests {
 		existingId = 1L;
 		nonExistingId = 1000L;
 		countTotalProducts = 25L;
+		productDTO = Factory.createProductDTO();
 	}
 	
 	@Test
@@ -47,6 +57,25 @@ public class ProductResourceIntegrationTests {
 		result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
 		result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
 		result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+	}
+	
+	@Test
+	public void updateShouldReturnProductWhenIdExists() throws Exception {
+		
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		String expectedName = productDTO.getName();
+		String expectedDescription = productDTO.getDescription();
+		
+		ResultActions result = 
+				mockMvc.perform(put("/products/{id}", existingId)
+						.content(jsonBody)
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").value(existingId));
+		result.andExpect(jsonPath("$.name").value(expectedName));
+		result.andExpect(jsonPath("$.description").value(expectedDescription));
 	}
 }
 
