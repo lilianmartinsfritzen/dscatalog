bd3c5b4f38943e9424b3172f4995c8d6a112cb8b
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-09-22 23:37:57 -0300

    Update test on the web layer.

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dscatalog.resources;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,6 +26,7 @@ import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.services.ProductService;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscatalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(ProductResource.class)
 public class ProductResourceTests {
@@ -33,6 +36,9 @@ public class ProductResourceTests {
 	
 	@MockBean
 	private ProductService service;
+	
+	@Autowired  // Não tem problema injetar o próprio obj, porque o ObjectMapper é so um obj auxiliar e não uma depência do ProductResource, desta forma não fere o princípio de teste unitário.  
+	private ObjectMapper objectMapper;
 	
 	private long existingId;
 	private long nonExistingId;
@@ -59,6 +65,10 @@ public class ProductResourceTests {
 		
 		when(service.findById(existingId)).thenReturn(productDTO);		
 		when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+		
+		// Quando usamos any os outros objs não podem ser argumentos simples, tipo existingId, neste caso usamos o argumento eq
+		when(service.update(eq(existingId), any())).thenReturn(productDTO);		
+		when(service.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
 	}
 	
 	@Test
@@ -97,6 +107,24 @@ public class ProductResourceTests {
 						.accept(MediaType.APPLICATION_JSON));
 		
 		result.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void updateShouldReturnProductWhenIdExists() throws Exception {
+		
+		// Transforma o obj ProductDTO, java, em um JSON
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		
+		ResultActions result = 
+				mockMvc.perform(put("/products/{id}", existingId)
+						.content(jsonBody)
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.name").exists());
+		result.andExpect(jsonPath("$.description").exists());
 	}
 }
 
