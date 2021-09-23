b8986287e589bd7236b5c998f3c0bd6217e7d6e7
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-09-23 11:21:14 -0300

    FindAllPaged return page when we inform page number and quantity of items. Tested.

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIntegrationTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIntegrationTests.java
@@ -5,11 +5,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @SpringBootTest
+@Transactional	// @Transactional fará o DB retornar ao estado original para que cada teste ocorra de forma independente.
 public class ProductServiceIntegrationTests {
 
 	@Autowired
@@ -46,17 +51,26 @@ public class ProductServiceIntegrationTests {
 		});
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	@Test
+	public void findAllPagedShouldReturnPageWhenPageNumberSizeNumber() {
+											  // estamos chamando a primeira página (0) com o tamanho de 10 produtos e nós temos 25 produtos no total.
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+							  // result.isEmpty retorna verdadeiro qdo o obj está vazio, mas estamos negando com assertFalse.
+		Assertions.assertFalse(result.isEmpty());
+							  // 0 é o valor que esperamos que seja o número da página e estamos comparando ao result.getNumber, que retorna o número da página.
+		Assertions.assertEquals(0, result.getNumber());
+							  // esperamos que a página venha com 10 objetos, comparamos com result.getSize que retorna o total de itens da página. 
+		Assertions.assertEquals(10, result.getSize());
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+	}
 }
+
+
+
+
+
+
+
+
